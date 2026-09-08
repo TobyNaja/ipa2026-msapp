bd90e41714c45d42df86d6134e335209cd2304b8
TobyNaja <toybbnn123@gmail.com>
2026-09-08 12:23:44 +0700

    fix pep8-3

--- web/app.py
+++ web/app.py
@@ -31,8 +31,7 @@ def add_router():
     password = request.form.get("password")
 
     if ip and username and password:
-        collection.insert_one({"ip": ip, "username": username,
-                               "password": password})
+        collection.insert_one({"ip": ip, "username": username, "password": password})
 
     return redirect(url_for("main"))
 
@@ -51,11 +50,9 @@ def delete_router():
 def router_detail(router_ip):
 
     routers = list(
-        db["interface_status"].find({"router_ip": router_ip})
-        .sort("timestamp", -1)
+        db["interface_status"].find({"router_ip": router_ip}).sort("timestamp", -1)
     )
-    return render_template("router_detail.html",
-                           routers=routers, router_ip=router_ip)
+    return render_template("router_detail.html", routers=routers, router_ip=router_ip)
 
 
 if __name__ == "__main__":
